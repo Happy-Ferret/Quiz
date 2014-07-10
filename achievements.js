4c13e16791ec4a36bffea33454a87314119e78e9
Happy-Ferret <MarkBauer@gmx.ch>
2014-07-10 22:45:44 +0200

    Added experimental clay.io achievement functionality.

---
+++ achievements.js
@@ -0,0 +1,11 @@
+Clay = {};
+Clay.gameKey = "";
+Clay.readyFunctions = [];
+Clay.ready = function( fn ) {
+    Clay.readyFunctions.push( fn );
+};
+( function() {
+    var clay = document.createElement("script"); clay.async = true;
+    clay.src = ( "https:" == document.location.protocol ? "https://" : "http://" ) + "clay.io/api/api.js"; 
+    var tag = document.getElementsByTagName("script")[0]; tag.parentNode.insertBefore(clay, tag);
+} )();
